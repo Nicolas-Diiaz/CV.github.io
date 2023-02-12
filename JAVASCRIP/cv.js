c5ae3f8e32a9c9230f1cdf6b56e436a9f2883916
Nicolas-Diiaz <125157124+Nicolas-Diiaz@users.noreply.github.com>
2023-02-12 14:42:16 -0300

    Add files via upload

---
+++ JAVASCRIP/cv.js
@@ -0,0 +1,28 @@
+function myFunction () {
+    document.getElementById('NombreyApellido').innerHTML="Matias Nicolas Diaz";
+}
+
+function myEdad () {
+    document.getElementById('Edad').innerHTML="25 Años";
+}
+
+function myEstadoCivil () {
+    document.getElementById('Estado_Civil').innerHTML="Soltero";
+}
+
+function myCorreo () {
+    document.getElementById('Correo_Electronico').innerHTML="matiasdiiaz@hotmail.com";
+}
+
+function myCelular () {
+    document.getElementById('Celular').innerHTML="+54 261-361-1777";
+}
+
+
+function myNacimiento () {
+    document.getElementById('Fecha_de_Nacimiento').innerHTML="22/04/1997";
+}
+
+function myNacionalidad () {
+    document.getElementById('Nacionali').innerHTML="Argentino";
+}
